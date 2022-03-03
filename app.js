b684c9e86dc109a330b106468e634f7d481fa07f
joshraiborde <joshraiborde@gmail.com>
2022-03-03 11:14:35 -0500

    testing out server, sending index.html

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ const app = express();
 const port = 3000;
 var now = new Date();
 
-app.get("/", (req, res){
-    res.send("Hello");
+app.get("/", (req, res)=>{
+
+    var today = new Date();
+    var currentDay = today.getDay()
+
+    if (currentDay === 6 || currentDay === 0) { // tests to see if it's a Sat or Sun
+        res.write("<h1>It's the Weekend because it is " + today.toUTCString() + "!</h1>")
+    } else {
+        res.sendFile(__dirname + "/index.html")
+    }
+
+
+    // res.send("Hello");
 });
 
 app.listen(port, ()=>{
