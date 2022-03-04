ff6e10711458f38dc0a156130d139eb02f385768
joshraiborde <joshraiborde@gmail.com>
2022-03-04 17:52:31 -0500

    aded boilerplate for app.use-bodyParser, added empty items array, added newListItems to res.render, added app.post

--- app.js
+++ app.js
@@ -5,39 +5,32 @@ const app = express();
 const port = 3000;
 var now = new Date();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   var today = new Date();
   var currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: currentDay is equal to: " + currentDay)
-  }
-  res.render("list", { kindOfDay: day });
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", (req, res) => {
+    var item = req.body.newItem;
+    items.push(item)
+//   console.log(item);
+res.redirect("/");
 });
 
 app.listen(port, () => {
