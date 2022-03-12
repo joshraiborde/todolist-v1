890bc67295d242a6b59f129028cf5edbbaefd012
joshraiborde <joshraiborde@gmail.com>
2022-03-12 14:44:24 -0500

    changed all the var to let

--- app.js
+++ app.js
@@ -3,33 +3,33 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
-var now = new Date();
+let now = new Date();
 
-var items = [];
-// var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = [];
+// let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  var today = new Date();
-  var currentDay = today.getDay();
+  let today = new Date();
+  let currentDay = today.getDay();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
     year: "numeric"
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", { kindOfDay: day, newListItems: items });
 });
 
 app.post("/", (req, res) => {
-    var item = req.body.newItem;
+    let item = req.body.newItem;
     items.push(item)
 //   console.log(item);
 res.redirect("/");
