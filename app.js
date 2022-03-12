571a294db36efa10ed4a8f7e9256bd5f115a27ea
joshraiborde <joshraiborde@gmail.com>
2022-03-12 15:19:43 -0500

    added app.use to add an express static for the public folder

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   let today = new Date();
   let currentDay = today.getDay();
